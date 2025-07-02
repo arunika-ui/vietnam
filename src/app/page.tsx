84c879544610692c61917ec3ba230be3f2f8f4e2
admin <arunikajain22@gmail.com>
2025-07-03 02:53:00 +0530

    directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-export default function Home() {
-  const router = useRouter();
-  router.push("/vietnam");
-  return <div className="w-full"></div>;
-}
