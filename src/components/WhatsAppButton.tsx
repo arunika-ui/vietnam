b4f574d017c8529c894250eebc97e5e7be8cacc7
arunika-ui <arunikajain22@gmail.com>
2025-07-23 17:11:34 +0530

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -10,7 +10,7 @@ export default function WhatsAppButton() {
       className="fixed bottom-5 right-5 z-50 w-[60px] h-[60px] bg-green-500 rounded-full flex items-center justify-center shadow-lg hover:scale-105 transition-transform duration-300"
     >
       <Image
-        src="/public/whatsapp-logo.avif"
+        src="/whatsapp-logo.avif"
         alt="WhatsApp"
         width={30}
         height={30}
