68fd330f012ea87a0a485ec330970ee50bac4d98
AwesomeSam <76945339+AwesomeSam9523@users.noreply.github.com>
2025-08-22 01:12:36 +0530

    Update BackgroundVideo.tsx

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -5,7 +5,7 @@ export default function BackgroundVideo() {
     <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
       <video
         className="w-full h-full object-cover"
-        src="/vietnam.mp4"
+        src="/vietnam/vietnam.mp4"
         autoPlay
         loop
         muted
